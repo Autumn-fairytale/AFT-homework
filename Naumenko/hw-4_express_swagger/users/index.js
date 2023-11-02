3f48fd33fc41b0a4fe1fda49f200bf7cfd6ea8ce
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-11-03 00:12:06 +0200

    UPD: index.js

--- Naumenko/hw-4_express_swagger/users/index.js
+++ Naumenko/hw-4_express_swagger/users/index.js
@@ -3,11 +3,11 @@ import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 // Отримати поточну директорію в ES6 модулі:
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// ============================================
 
-console.log("Поточна директорія:", __dirname);
+// console.log("Поточна директорія:", __dirname);
 
 const getAll = async () => {
   const data = await fs.readFile(`${__dirname}/users.json`, "utf-8");
