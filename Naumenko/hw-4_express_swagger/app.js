f78a448bda8dc8b6475fad8df108a4872493899e
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2023-11-04 00:00:30 +0200

    ADD: get by Id

--- Naumenko/hw-4_express_swagger/app.js
+++ Naumenko/hw-4_express_swagger/app.js
@@ -1,4 +1,5 @@
-import getAll from "./users/index.js";
+import getAll from "./controllers/users/getAllUsers/index.js";
+import { getById } from "./controllers/users/getById/index.js";
 
 const invokeAction = async ({ action, id, name, login, password, status }) => {
   console.log(action);
@@ -6,7 +7,11 @@ const invokeAction = async ({ action, id, name, login, password, status }) => {
     case "read":
       const allUsers = await getAll();
       return console.log("allUsers:", allUsers);
+    case "getById":
+      const user = await getById(id);
+      console.log("user:", user);
   }
 };
 
-invokeAction({ action: "read" });
+// invokeAction({ action: "read" });
+invokeAction({ action: "getById", id: 9 });
--- Naumenko/hw-4_express_swagger/controllers/users/getAllUsers/index.js
+++ Naumenko/hw-4_express_swagger/controllers/users/getAllUsers/index.js
@@ -1,16 +1,17 @@
 import fs from "fs/promises";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
+import { dirname, join } from "path";
 
 // Отримати поточну директорію в ES6 модулі:
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 // ============================================
 
+const usersPath = join(__dirname, "users.json");
 // console.log("Поточна директорія:", __dirname);
 
 const getAll = async () => {
-  const data = await fs.readFile(`${__dirname}/users.json`, "utf-8");
+  const data = await fs.readFile(usersPath);
   return JSON.parse(data, null, 2);
 };
 
